bd3a17fc55bf56735118f94dcc822c0b316e01f3
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2021-01-21 17:30:06 -0700

    first commit

---
+++ src/main/java/com/sujoy/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.sujoy.ecommerce.config;
+
+import com.sujoy.ecommerce.entity.Product;
+import com.sujoy.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        // disable the methods for Product POST, PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType((Product.class))
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // disable the methods for ProductCategpry POST, PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType((ProductCategory.class))
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
+}
